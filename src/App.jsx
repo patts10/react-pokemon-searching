07da5497f97704d139368577663175ea1502875c
Christian Mendoza <jonathan.mendoza@bennu.cl>
2023-02-11 21:36:31 -0500

    feat: create mocks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css'
+import pokemonResponse from './mocks/with-results.json'
 
 function App () {
+  const pokemon = pokemonResponse
+  const hasPokemon = !(typeof pokemon === 'string')
+
   return (
     <div className='page'>
 
@@ -13,7 +17,19 @@ function App () {
       </header>
 
       <main>
-        results are going to be here
+        {
+          hasPokemon
+            ? (
+              <ul>
+                <li>
+                  <h3>{pokemon.name}</h3>
+                  <p>{pokemon.weight}</p>
+                  <img src={pokemon.sprites.other.home.front_default} alt={pokemon.name} />
+                </li>
+              </ul>
+              )
+            : <h3>{`Pokemon ${'xxx'} not found`}</h3>
+        }
       </main>
     </div>
   )
